2983a904b3db76657479b7923ba898044a698a12
dineshkumar7031 <141705801+dineshkumar7031@users.noreply.github.com>
2024-09-12 07:47:29 +0530

    Update output.tf

--- EKS_Terraform/output.tf
+++ EKS_Terraform/output.tf
@@ -3,13 +3,13 @@ output "cluster_id" {
 }
 
 output "node_group_id" {
-  value = aws_eks_node_group.devopsshack.id
+  value = aws_eks_node_group.dinesh.id
 }
 
 output "vpc_id" {
-  value = aws_vpc.devopsshack_vpc.id
+  value = aws_vpc.dinesh_vpc.id
 }
 
 output "subnet_ids" {
-  value = aws_subnet.devopsshack_subnet[*].id
+  value = aws_subnet.dinesh_subnet[*].id
 }
