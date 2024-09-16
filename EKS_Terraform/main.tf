cf2fa0552df1d7e411763093cc1c5b013e1749d9
dineshkumar7031 <141705801+dineshkumar7031@users.noreply.github.com>
2024-09-16 08:07:35 +0530

    Update main.tf

--- EKS_Terraform/main.tf
+++ EKS_Terraform/main.tf
@@ -2,55 +2,55 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_vpc" "dinesh_vpc" {
+resource "aws_vpc" "dinu_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "dinesh-vpc"
+    Name = "dinu-vpc"
   }
 }
 
-resource "aws_subnet" "dinesh_subnet" {
+resource "aws_subnet" "dinu_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.dinesh_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.dinesh_vpc.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.dinu_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.dinu_vpc.cidr_block, 8, count.index)
   availability_zone       = element(["ap-south-1a", "ap-south-1b"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "dinesh-subnet-${count.index}"
+    Name = "dinu-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "dinesh_igw" {
-  vpc_id = aws_vpc.dinesh_vpc.id
+resource "aws_internet_gateway" "dinu_igw" {
+  vpc_id = aws_vpc.dinu_vpc.id
 
   tags = {
-    Name = "dinesh-igw"
+    Name = "dinu-igw"
   }
 }
 
-resource "aws_route_table" "dinesh_route_table" {
-  vpc_id = aws_vpc.dinesh_vpc.id
+resource "aws_route_table" "dinu_route_table" {
+  vpc_id = aws_vpc.dinu_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.dinesh_igw.id
+    gateway_id = aws_internet_gateway.dinu_igw.id
   }
 
   tags = {
-    Name = "dinesh-route-table"
+    Name = "dinu-route-table"
   }
 }
 
 resource "aws_route_table_association" "a" {
   count          = 2
-  subnet_id      = aws_subnet.dinesh_subnet[count.index].id
-  route_table_id = aws_route_table.dinesh_route_table.id
+  subnet_id      = aws_subnet.dinu_subnet[count.index].id
+  route_table_id = aws_route_table.dinu_route_table.id
 }
 
-resource "aws_security_group" "dinesh_cluster_sg" {
-  vpc_id = aws_vpc.dinesh_vpc.id
+resource "aws_security_group" "dinu_cluster_sg" {
+  vpc_id = aws_vpc.dinu_vpc.id
 
   egress {
     from_port   = 0
@@ -64,8 +64,8 @@ resource "aws_security_group" "dinesh_cluster_sg" {
   }
 }
 
-resource "aws_security_group" "dinesh_node_sg" {
-  vpc_id = aws_vpc.dinesh_vpc.id
+resource "aws_security_group" "dinu_node_sg" {
+  vpc_id = aws_vpc.dinu_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,25 +82,25 @@ resource "aws_security_group" "dinesh_node_sg" {
   }
 
   tags = {
-    Name = "dinesh-node-sg"
+    Name = "dinu-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "dinesh" {
-  name     = "dinesh-cluster"
-  role_arn = aws_iam_role.dinesh_cluster_role.arn
+resource "aws_eks_cluster" "dinu" {
+  name     = "dinu-cluster"
+  role_arn = aws_iam_role.dinu_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.dinesh_subnet[*].id
-    security_group_ids = [aws_security_group.dinesh_cluster_sg.id]
+    subnet_ids         = aws_subnet.dinu_subnet[*].id
+    security_group_ids = [aws_security_group.dinu_cluster_sg.id]
   }
 }
 
-resource "aws_eks_node_group" "dinesh" {
-  cluster_name    = aws_eks_cluster.dinesh.name
-  node_group_name = "dinesh-node-group"
-  node_role_arn   = aws_iam_role.dinesh_node_group_role.arn
-  subnet_ids      = aws_subnet.dinesh_subnet[*].id
+resource "aws_eks_node_group" "dinu" {
+  cluster_name    = aws_eks_cluster.dinu.name
+  node_group_name = "dinu-node-group"
+  node_role_arn   = aws_iam_role.dinu_node_group_role.arn
+  subnet_ids      = aws_subnet.dinu_subnet[*].id
    
   scaling_config {
     desired_size = 3
@@ -112,12 +112,12 @@ resource "aws_eks_node_group" "dinesh" {
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.dinesh_node_sg.id]
+    source_security_group_ids = [aws_security_group.dinu_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "dinesh_cluster_role" {
-  name = "dinesh-cluster-role"
+resource "aws_iam_role" "dinu_cluster_role" {
+  name = "dinu-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -135,13 +135,13 @@ resource "aws_iam_role" "dinesh_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "dinesh_cluster_role_policy" {
-  role       = aws_iam_role.dinesh_cluster_role.name
+resource "aws_iam_role_policy_attachment" "dinu_cluster_role_policy" {
+  role       = aws_iam_role.dinu_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "dinesh_node_group_role" {
-  name = "dinesh-node-group-role"
+resource "aws_iam_role" "dinu_node_group_role" {
+  name = "dinu-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -160,16 +160,16 @@ EOF
 }
 
 resource "aws_iam_role_policy_attachment" "dinesh_node_group_role_policy" {
-  role       = aws_iam_role.dinesh_node_group_role.name
+  role       = aws_iam_role.dinu_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "dinesh_node_group_cni_policy" {
-  role       = aws_iam_role.dinesh_node_group_role.name
+resource "aws_iam_role_policy_attachment" "dinu_node_group_cni_policy" {
+  role       = aws_iam_role.dinu_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "dinesh_node_group_registry_policy" {
-  role       = aws_iam_role.dinesh_node_group_role.name
+resource "aws_iam_role_policy_attachment" "dinu_node_group_registry_policy" {
+  role       = aws_iam_role.dinu_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
