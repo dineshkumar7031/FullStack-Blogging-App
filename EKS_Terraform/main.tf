1b8e9e5833db2a9fbc04a60b3b3f0a31aeec202b
dineshkumar7031 <141705801+dineshkumar7031@users.noreply.github.com>
2024-09-25 07:25:29 +0530

    Update main.tf

--- EKS_Terraform/main.tf
+++ EKS_Terraform/main.tf
@@ -2,55 +2,55 @@ provider "aws" {
   region = "us-west-1"
 }
 
-resource "aws_vpc" "dev_vpc" {
+resource "aws_vpc" "uat_vpc" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "dev-vpc"
+    Name = "uat-vpc"
   }
 }
 
-resource "aws_subnet" "dev_subnet" {
+resource "aws_subnet" "uat_subnet" {
   count = 2
-  vpc_id                  = aws_vpc.dev_vpc.id
-  cidr_block              = cidrsubnet(aws_vpc.dev_vpc.cidr_block, 8, count.index)
+  vpc_id                  = aws_vpc.uat_vpc.id
+  cidr_block              = cidrsubnet(aws_vpc.uat_vpc.cidr_block, 8, count.index)
   availability_zone       = element(["us-west-1b", "us-west-1c"], count.index)
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "dev-subnet-${count.index}"
+    Name = "uat-subnet-${count.index}"
   }
 }
 
-resource "aws_internet_gateway" "dev_igw" {
-  vpc_id = aws_vpc.dev_vpc.id
+resource "aws_internet_gateway" "uat_igw" {
+  vpc_id = aws_vpc.uat_vpc.id
 
   tags = {
-    Name = "dev-igw"
+    Name = "uat-igw"
   }
 }
 
-resource "aws_route_table" "dev_route_table" {
-  vpc_id = aws_vpc.dev_vpc.id
+resource "aws_route_table" "uat_route_table" {
+  vpc_id = aws_vpc.uat_vpc.id
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.dev_igw.id
+    gateway_id = aws_internet_gateway.uat_igw.id
   }
 
   tags = {
-    Name = "dev-route-table"
+    Name = "uat-route-table"
   }
 }
 
 resource "aws_route_table_association" "a" {
   count          = 2
-  subnet_id      = aws_subnet.dev_subnet[count.index].id
-  route_table_id = aws_route_table.dev_route_table.id
+  subnet_id      = aws_subnet.uat_subnet[count.index].id
+  route_table_id = aws_route_table.uat_route_table.id
 }
 
-resource "aws_security_group" "dev_cluster_sg" {
-  vpc_id = aws_vpc.dev_vpc.id
+resource "aws_security_group" "uat_cluster_sg" {
+  vpc_id = aws_vpc.uat_vpc.id
 
   egress {
     from_port   = 0
@@ -60,12 +60,12 @@ resource "aws_security_group" "dev_cluster_sg" {
   }
 
   tags = {
-    Name = "dev-cluster-sg"
+    Name = "uat-cluster-sg"
   }
 }
 
-resource "aws_security_group" "dev_node_sg" {
-  vpc_id = aws_vpc.dev_vpc.id
+resource "aws_security_group" "uat_node_sg" {
+  vpc_id = aws_vpc.uat_vpc.id
 
   ingress {
     from_port   = 0
@@ -82,25 +82,25 @@ resource "aws_security_group" "dev_node_sg" {
   }
 
   tags = {
-    Name = "dev-node-sg"
+    Name = "uat-node-sg"
   }
 }
 
-resource "aws_eks_cluster" "dev" {
+resource "aws_eks_cluster" "uat" {
   name     = "dev-cluster"
-  role_arn = aws_iam_role.dev_cluster_role.arn
+  role_arn = aws_iam_role.uat_cluster_role.arn
 
   vpc_config {
-    subnet_ids         = aws_subnet.dev_subnet[*].id
-    security_group_ids = [aws_security_group.dev_cluster_sg.id]
+    subnet_ids         = aws_subnet.uat_subnet[*].id
+    security_group_ids = [aws_security_group.uat_cluster_sg.id]
   }
 }
 
-resource "aws_eks_node_group" "dev" {
-  cluster_name    = aws_eks_cluster.dev.name
-  node_group_name = "dev-node-group"
-  node_role_arn   = aws_iam_role.dev_node_group_role.arn
-  subnet_ids      = aws_subnet.dev_subnet[*].id
+resource "aws_eks_node_group" "uat" {
+  cluster_name    = aws_eks_cluster.uat.name
+  node_group_name = "uat-node-group"
+  node_role_arn   = aws_iam_role.uat_node_group_role.arn
+  subnet_ids      = aws_subnet.uat_subnet[*].id
    
   scaling_config {
     desired_size = 3
@@ -112,12 +112,12 @@ resource "aws_eks_node_group" "dev" {
 
   remote_access {
     ec2_ssh_key = var.ssh_key_name
-    source_security_group_ids = [aws_security_group.dev_node_sg.id]
+    source_security_group_ids = [aws_security_group.uat_node_sg.id]
   }
 }
 
-resource "aws_iam_role" "dev_cluster_role" {
-  name = "dev-cluster-role"
+resource "aws_iam_role" "uat_cluster_role" {
+  name = "uat-cluster-role"
 
   assume_role_policy = <<EOF
 {
@@ -135,13 +135,13 @@ resource "aws_iam_role" "dev_cluster_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "dev_cluster_role_policy" {
-  role       = aws_iam_role.dev_cluster_role.name
+resource "aws_iam_role_policy_attachment" "uat_cluster_role_policy" {
+  role       = aws_iam_role.uat_cluster_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
 
-resource "aws_iam_role" "dev_node_group_role" {
-  name = "dev-node-group-role"
+resource "aws_iam_role" "uat_node_group_role" {
+  name = "uat-node-group-role"
 
   assume_role_policy = <<EOF
 {
@@ -159,17 +159,17 @@ resource "aws_iam_role" "dev_node_group_role" {
 EOF
 }
 
-resource "aws_iam_role_policy_attachment" "dev_node_group_role_policy" {
-  role       = aws_iam_role.dev_node_group_role.name
+resource "aws_iam_role_policy_attachment" "uat_node_group_role_policy" {
+  role       = aws_iam_role.uat_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
 }
 
-resource "aws_iam_role_policy_attachment" "dev_node_group_cni_policy" {
-  role       = aws_iam_role.dev_node_group_role.name
+resource "aws_iam_role_policy_attachment" "uat_node_group_cni_policy" {
+  role       = aws_iam_role.uat_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
 }
 
-resource "aws_iam_role_policy_attachment" "dev_node_group_registry_policy" {
-  role       = aws_iam_role.dev_node_group_role.name
+resource "aws_iam_role_policy_attachment" "uat_node_group_registry_policy" {
+  role       = aws_iam_role.uat_node_group_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
 }
